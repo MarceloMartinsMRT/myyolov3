56a7d8593cd807df9bc0babb77b297323f923eca
MarceloMartinsMRT <77979337+MarceloMartinsMRT@users.noreply.github.com>
2022-04-08 13:04:45 -0400

    update test send to httpbin

--- workkickup.py
+++ workkickup.py
@@ -515,4 +515,27 @@ for i in range(len(img_array)):
   out.write(img_array[i])
   
 out.release()
-print('finished')
+print('finished create video')
+
+print('pausa 5')
+time.sleep(10.0)
+
+
+import requests
+test_url = "http://httpbin.org/post"
+test_files = {
+    "test_file_1": open("finalcount.bin", "rb"),
+    "test_file_2": open("finalvideo.mp4", "rb")
+    
+}
+
+test_response = requests.post(test_url, files = test_files)
+
+if test_response.ok:
+    print("Upload completed successfully!")
+    print(test_response.text)
+else:
+    print("Something went wrong!")
+
+
+print('finished send')
